bc3ba77d4547291a6886c51ad4a46ccebd24c103
n1tr0xs <niko4pok@gmail.com>
2025-09-15 19:24:06 +0300

    logging improved

--- main.py
+++ main.py
@@ -6,35 +6,45 @@ from excel import ExcelBook
 import utils
 from schemas import Bond, SearchCriteria
 
-logger = logging.getLogger("Main")
+# Logger setup
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(
+            f"{datetime.datetime.now().strftime("%d.%m.%Y")}.log",
+            mode="w",
+            encoding="utf-8",
+        ),
+        logging.StreamHandler(),
+    ],
+)
 
+logger = logging.getLogger("Main")
 
 def main():
     # Search criteria setup
     INF = float("inf")
     search_criteria: SearchCriteria = SearchCriteria(
-        min_bond_yield=20/.87,
+        min_bond_yield=20 / 0.87,
         max_bond_yield=INF,
         min_days_to_maturity=1,
         max_days_to_maturity=INF,
         face_units=None,
     )
 
-    # Logger setup
-    logging.basicConfig(
-        filename=f"{datetime.datetime.now().strftime("%d.%m.%Y")}.log",
-        filemode="w",
-        level=logging.INFO,
-    )
-
     # Main
-    logger.info(f"Начало работы: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}")
+    logger.info(
+        f"Начало работы: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}"
+    )
     moex_api = MOEX_API()
     bonds: list[Bond] = moex_api.get_bonds()
     bonds: list[Bond] = utils.filter_bonds(bonds, search_criteria)
     bonds: list[Bond] = utils.add_credit_scores(bonds)
     ExcelBook().write(bonds)
-    logger.info(f"Конец работы: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}")
+    logger.info(
+        f"Конец работы: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}"
+    )
+
 
 if __name__ == "__main__":
     main()
